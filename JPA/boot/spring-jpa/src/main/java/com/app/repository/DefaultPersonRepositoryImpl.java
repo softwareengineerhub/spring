c19d0795931c92a0e68cce032f948118bcbee934
denis4321 <denis4321@ukr.net>
2018-09-11 18:23:32 +0300

    Spring Data Jpa

---
+++ JPA/boot/spring-jpa/src/main/java/com/app/repository/DefaultPersonRepositoryImpl.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.repository;
+
+import com.app.model.Person;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Denys.Prokopiuk
+ */
+@Repository(value = "default")
+public class DefaultPersonRepositoryImpl implements PersonRepository {
+
+    private List<Person> persons;
+
+    @PostConstruct
+    public void init() {
+        persons = new ArrayList<>();
+        persons.add(null);
+    }
+
+    @Override
+    public int count() {
+        return persons.size();
+    }
+
+    @Override
+    public Person get(int id) {
+        return persons.get(id);
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return persons;
+    }
+
+    @Override
+    public void save(Person person) {
+        persons.add(person);
+        person.setId(persons.size() - 1);
+    }
+
+    @Override
+    public void saveAll(List<Person> persons) {
+        this.persons.addAll(persons);
+    }
+
+    @Override
+    public void update(int id, Person candidat) {
+        persons.set(id, candidat);
+    }
+
+    @Override
+    public void remove(int id) {
+        persons.remove(id);
+    }
+
+    @Override
+    public void removeAll() {
+        persons.clear();
+    }
+
+}
---
+++ JPA/boot/spring-jpa/src/main/java/com/app/service/PersonServiceImpl.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.service;
+
+import com.app.model.Person;
+import com.app.repository.PersonRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Denys.Prokopiuk
+ */
+@Service
+public class PersonServiceImpl implements PersonService {
+
+    @Autowired
+    //@Qualifier(value = "default")
+    @Qualifier(value = "jpaDefault")
+    private PersonRepository personRepository;
+
+    @Override
+    public long count() {
+        return personRepository.count();
+    }
+
+    @Override
+    public Person get(int id) {
+        return personRepository.get(id);
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return personRepository.getAll();
+    }
+
+    @Override
+    public void save(Person person) {
+        personRepository.save(person);
+    }
+
+    @Override
+    public void saveAll(List<Person> persons) {
+        personRepository.saveAll(persons);
+    }
+
+    @Override
+    public void update(int id, Person candidat) {
+        personRepository.update(id, candidat);
+    }
+
+    @Override
+    public void remove(int id) {
+        personRepository.remove(id);
+    }
+
+    @Override
+    public void removeAll() {
+        personRepository.removeAll();
+    }
+
+}
