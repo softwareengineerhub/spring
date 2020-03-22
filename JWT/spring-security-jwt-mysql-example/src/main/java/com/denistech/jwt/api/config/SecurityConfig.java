2de168c6a5fff204e0f67739777cfcb12e5157bd
softwareengineerhub <denis.prokopiuk@yahoo.com>
2020-03-22 21:07:19 +0200

    Commit

--- JWT/spring-security-jwt-mysql-example/src/main/java/com/denistech/jwt/api/config/SecurityConfig.java
+++ JWT/spring-security-jwt-mysql-example/src/main/java/com/denistech/jwt/api/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -42,6 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
+        PasswordEncoder passwordEncoder = new Argon2PasswordEncoder();
+      //  return passwordEncoder;
         return NoOpPasswordEncoder.getInstance();
     }
 
