6d83aab6985b6e028331077a43c9673701a33c12
softwareengineerhub <denis.prokopiuk@yahoo.com>
2020-03-22 20:16:37 +0200

    Commit

---
+++ JWT/spring-security-jwt-mysql-example/src/main/java/com/denistech/jwt/api/SpringSecutirtyGwtExampleApplication.java
@@ -0,0 +1,38 @@
+package com.denistech.jwt.api;
+
+import com.denistech.jwt.api.entity.User;
+import com.denistech.jwt.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+//@EnableJpaRepositories
+public class SpringSecutirtyGwtExampleApplication {
+
+    @Autowired
+    private UserRepository repository;
+
+    @PostConstruct
+    public void initUsers() {
+        List<User> users = Stream.of(
+                new User(101, "javatechie", "password", "javatechie@gmail.com"),
+                new User(102, "user1", "pwd1", "user1@gmail.com"),
+                new User(103, "user2", "pwd2", "user2@gmail.com"),
+                new User(104, "user3", "pwd3", "user3@gmail.com"),
+                new User(105, "denis", "1", "user4@gmail.com")
+        ).collect(Collectors.toList());
+        repository.saveAll(users);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecutirtyGwtExampleApplication.class, args);
+    }
+
+}
---
+++ JWT/spring-security-jwt-mysql-example/src/main/resources/README.txt
@@ -0,0 +1,16 @@
+1) http://localhost:9192/welcome
+POST:
+{"userName":"denis","password":"1"}
+
+response:
+eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZW5pcyIsImlhdCI6MTU4NDg5MjI4MCwiZXhwIjoxNTg0ODkyMzQwfQ.gSLht4nw3neP8iXq9YS-_Z_ubnNGKXtm76OfS1FWJKE
+
+
+
+2) http://localhost:9192/authenticate
+GET:
+Header:
+Authorization: MyBearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZW5pcyIsImlhdCI6MTU4NDg5MjI4MCwiZXhwIjoxNTg0ODkyMzQwfQ.gSLht4nw3neP8iXq9YS-_Z_ubnNGKXtm76OfS1FWJKe
+
+response:
+page content
