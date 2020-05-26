0be73aea91ffbeae1e82992a577f7c766b0d5e9a
softwareengineerhub <denis.prokopiuk@yahoo.com>
2020-05-26 17:16:53 +0300

    Commit

--- FeignHttpClient/spring-feign-server-side/src/main/java/com/app/controller/SimpleController.java
+++ FeignHttpClient/spring-feign-server-side/src/main/java/com/app/controller/SimpleController.java
@@ -6,10 +6,12 @@
 package com.app.controller;
 
 import com.app.model.Person;
+import com.app.model.PersonResponse;
 import com.app.repository.PersonRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,11 +26,41 @@ public class SimpleController {
 
     @Autowired
     private PersonRepository personRepository;
-
+    
     @PostMapping("/persons")
     public void add(@RequestBody Person person) {
         personRepository.getList().add(person);
     }
+
+    @PostMapping("/persons2")
+    public PersonResponse add2(@RequestBody Person person) {
+        personRepository.getList().add(person);
+        PersonResponse personResponse = new PersonResponse();
+        personResponse.setStatus("OK");
+        return personResponse;
+    }
+    
+    @PatchMapping("/persons3")
+    public PersonResponse update(@RequestBody Person person) {
+        int index = personRepository.getList().indexOf(person);
+        if(index!=-1){
+            
+        }
+        add(person);
+        PersonResponse personResponse = new PersonResponse();
+        personResponse.setStatus("OK");
+        return personResponse;
+    }
+    
+    @PatchMapping("/persons4")
+    public PersonResponse update2(@RequestBody Person person) {
+        personRepository.getList().add(person);
+        PersonResponse personResponse = new PersonResponse();
+        personResponse.setStatus("OK");
+        return personResponse;
+    }
+    
+    
 
     @GetMapping("/persons/{id}")
     public Person getById(@PathVariable int id) {
