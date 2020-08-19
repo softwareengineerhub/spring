53baed1f6f00999ec9d291a97ef628b7c676924b
softwareengineerhub <denis.prokopiuk@yahoo.com>
2020-08-19 12:37:58 +0300

    Commit

--- WebSocket/spring-boot2-websocket-app/src/main/java/com/app/config/AppConfig.java
+++ WebSocket/spring-boot2-websocket-app/src/main/java/com/app/config/AppConfig.java
@@ -18,7 +18,7 @@ public class AppConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/ws-endpoint").withSockJS();
     }
 
 }
