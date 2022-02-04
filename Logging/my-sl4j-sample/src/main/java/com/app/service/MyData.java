4c045d0a41e7aa31d23a854ace9bfe6ba052d884
softwareengineerhub <denis.prokopiuk@yahoo.com>
2022-02-04 10:29:50 +0200

    Commit

---
+++ Logging/my-sl4j-sample/src/main/java/com/app/service/MyData.java
@@ -0,0 +1,13 @@
+package com.app.service;
+
+public class MyData {
+    public Long data;
+
+    public Long getData() {
+        return data;
+    }
+
+    public void setData(Long data) {
+        this.data = data;
+    }
+}
--- Logging/my-sl4j-sample/src/main/java/com/app/service/MyService.java
+++ Logging/my-sl4j-sample/src/main/java/com/app/service/MyService.java
@@ -3,6 +3,8 @@ package com.app.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 @Slf4j
 public class MyService {
@@ -15,6 +17,15 @@ public class MyService {
         log.info("An INFO Message");
         log.warn("A WARN Message");
         log.error("An ERROR Message");
+
+        log.info("Hello, {}", "Denis");
+        MyData md = new MyData();
+        log.info("Data, {}", md.data);
+
+        MyData md1 = new MyData();
+        md1.setData(10L);
+        Object erid = new Object();
+        log.info("Reassociating recovery id from non existing to {}; ExternalRecoveryId={}", md1.getData(), erid.toString());
     }
 
 }
